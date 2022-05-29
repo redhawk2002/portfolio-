e9075e6c926a187045c9e6a0da1c7c85e7ba9d02
Sandeepan Kalita <77795912+redhawk2002@users.noreply.github.com>
2022-05-29 11:42:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+document.querySelector(".nav_links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
